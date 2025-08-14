c67aaceff750462baba9b3bc30a36cc8ac2f4b02
bellatrijuliana <bellatrijuliana01@gmail.com>
2025-08-14 17:28:55 +0700

    fix bug

--- factory/factory.go
+++ factory/factory.go
@@ -11,21 +11,16 @@ import (
 	"github.com/bellatrijuliana/agoratix-app/features/event/service"
 )
 
-// Initialize akan membuat dan menyambungkan semua komponen, lalu mengembalikan instance Echo
 func Initialize(db *sqlx.DB) *echo.Echo {
-	// Inisialisasi semua lapisan dari bawah ke atas
 	repo := repository.NewRepository(db)
 	srv := service.NewService(repo)
 	h := delivery.NewHandler(srv)
 
-	// Buat instance Echo
 	e := echo.New()
 
-	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Definisikan Rute Endpoint
 	e.GET("/GetEventList", h.GetEventList)
 	e.POST("/GetEventByID", h.GetEventByID)
 	e.POST("/InsertEvent", h.InsertEvent)
